643c68a84cdb06c6ed1745d78123aecc2c4cd9de
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-03-04 13:41:29 -0500

    commit tree 8c3c907ed43ae32595daff0c1565531dccac0177

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
+// function hasTargetSum(array, target) {
+//   array.forEach(num => {
+//     let difference;
+//     difference = target - num;
+//     array.forEach(num2 => {
+//       if(num2 === difference) {
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     })
+//   })
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+    const difference = target - array[i];
+    for(let j = i + 1; j < array.length; j++) {
+      if(difference === array[j]) {
+        return true;
+      }
+    }
+  } return false;
 }
 
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) return true;
+//     }
+//   }
+//   return false;
+// }
+
 /* 
-  Write the Big O time complexity of your function here
+  0(n) for the first forEach, plus another forEach 0(n) making it quadratic, no good.
+  Two loops are needed.
 */
 
 /* 
-  Add your pseudocode here
+  Loop over the array
+    find the difference between the target and element (10-3 =7)
+    if difference === the element
+      return true
+      stop running once it's true
+    else return false
 */
 
 /*
@@ -16,7 +53,7 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+//   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
